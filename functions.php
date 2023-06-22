f683a9d4dbfbe6f2320cb315e8c0d22524155db9
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-06-22 21:22:39 +0200

    Flowers rotation animation

--- functions.php
+++ functions.php
@@ -4,6 +4,13 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
+//Add custom javascript
+function koukaki_scripts() {
+    wp_enqueue_script('koukaki_script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ),'',true);
+}
+add_action( 'wp_enqueue_scripts', 'koukaki_scripts');
+
+
 // Get customizer options form parent theme
 if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
