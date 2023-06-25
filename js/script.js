0e160927dd50f2a70baf3a4a60dfcc322a95ed1b
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-06-25 11:56:42 +0200

    Titles animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('test')
+//Titles animation
+
+const titlesToAnimate = document.querySelectorAll(".title-to-animate");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("animate-title", entry.isIntersecting);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target)
+      }
+    });
+  },
+  { threshold: 1 }
+);
+
+titlesToAnimate.forEach((title) => {
+    observer.observe(title);
+  });
+
+
