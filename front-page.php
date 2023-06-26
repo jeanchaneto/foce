4660e140fef8be9c119ef7701470048f5d8fdb2d
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-06-26 12:03:39 +0200

    Add charcater swipper slides

--- front-page.php
+++ front-page.php
@@ -28,27 +28,34 @@ get_header();
         <article id="characters">
             <div class="main-character">
                 <h3>Les personnages</h3>
-                <?php
-                $main_character = $characters_query->posts[0];
-                echo '<figure>';
-                echo get_the_post_thumbnail($main_character->ID, 'full');
-                echo '<figcaption>' . $main_character->post_title . '</figcaption>';
-                echo '</figure>';
-                $characters_query->next_post();
-                ?>
-            </div>
-            <div class="other-characters">
-                <?php
-                while ($characters_query->have_posts()) {
-                    $characters_query->the_post();
-                    echo '<figure>';
-                    echo get_the_post_thumbnail(get_the_ID(), 'full');
-                    echo '<figcaption>';
-                    the_title();
-                    echo '</figcaption>';
-                    echo '</figure>';
-                }
-                ?>
+                <div class="swiper mySwiper">
+                    <div class="swiper-wrapper">
+                        <?php
+                        $main_character = $characters_query->posts[0];
+                        echo '<div class="swiper-slide">';
+                        echo '<figure>';
+                        echo get_the_post_thumbnail($main_character->ID, 'full');
+                        echo '<figcaption>' . $main_character->post_title . '</figcaption>';
+                        echo '</figure>';
+                        echo '</div>';
+                        $characters_query->next_post();
+                        ?>
+                        <?php
+                        while ($characters_query->have_posts()) {
+                            $characters_query->the_post();
+                            echo '<div class="swiper-slide">';
+                            echo '<figure>';
+                            echo get_the_post_thumbnail(get_the_ID(), 'full');
+                            echo '<figcaption>';
+                            the_title();
+                            echo '</figcaption>';
+                            echo '</figure>';
+                            echo '</div>';
+                        }
+                        ?>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
             </div>
         </article>
         <article id="place">
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 //Titles animation
 
 const titlesToAnimate = document.querySelectorAll(".title-to-animate");
@@ -18,4 +19,27 @@ titlesToAnimate.forEach((title) => {
     observer.observe(title);
   });
 
+  const logo = document.getElementById('logo');
+ ;
+
+//Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  
+});
 
